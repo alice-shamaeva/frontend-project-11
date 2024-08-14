f2262dc97e0b087607586f7486686962e9b73c7e
alice-shamaeva <alice.shamaeva@gmail.com>
2024-08-14 16:57:44 +0400

    webpack.config fixing

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,18 @@ const config = {
         { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         {
           test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  quietDeps: true,
+                },
+              },
+            },
+          ],
         },
       ],
     },
